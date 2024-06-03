9b21cb3ec7ae34721bec8347220f78033a72acc8
J0w03L <git@j0w03l.me>
2024-06-03 06:44:08 +0100

    Serverlogging test

--- src/main/java/com/data_stream/proximitytextchat/Events/chatEventHandler.java
+++ src/main/java/com/data_stream/proximitytextchat/Events/chatEventHandler.java
@@ -15,22 +15,36 @@ import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.server.ServerLifecycleHooks;
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
 
 import java.awt.*;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class chatEventHandler {
     @SubscribeEvent
     public static void chatEvent(ServerChatEvent event) {
         final BlockPos playerPos = event.getPlayer().blockPosition();
         final PlayerList playerList = ServerLifecycleHooks.getCurrentServer().getPlayerList();
+        List<String> playersHeardFull = new ArrayList<String>();
+        List<String> playersHeardPartial = new ArrayList<String>();
 
         for (int i = 0; i < playerList.getPlayers().size(); i++) {
             final double distance = Math.sqrt(playerList.getPlayers().get(i).blockPosition().distSqr(playerPos));
             if (distance <= ProxTextConfig.PROX_RANGE.get()) {
+                if (playerList.getPlayers().get(i).getUUID() != event.getPlayer().getUUID()) {
+                    playersHeardFull.add(playerList.getPlayers().get(i).getName().getString());
+                }
+
                 playerList.getPlayers().get(i).sendMessage(new TextComponent("<" + event.getPlayer().getName().getString() + "> " + event.getMessage().toString()), ChatType.SYSTEM, Util.NIL_UUID);
             }
             if (distance >= ProxTextConfig.PROX_RANGE.get() && distance <= ProxTextConfig.DIST_RANGE.get() && ProxTextConfig.TOGGLEDISTORTION.get()) {
+                if (playerList.getPlayers().get(i).getUUID() != event.getPlayer().getUUID()) {
+                    playersHeardPartial.add(playerList.getPlayers().get(i).getName().getString());
+                }
+
                 String[] words = event.getMessage().toString().split("\\s+");
                 String newmessage = "";
                 Random rand = new Random();
@@ -52,6 +66,8 @@ public class chatEventHandler {
                 playerList.getPlayers().get(i).sendMessage(new TextComponent("<" + event.getPlayer().getName().getString() + "> " + newmessage), ChatType.SYSTEM, Util.NIL_UUID);
             }
         }
+
+        LogUtils.getLogger().info("<" + event.getPlayer().getName().getString() + ">[" + String.join(", ", playersHeardFull) + "](" + String.join(", ", playersHeardPartial) + ") " + event.getMessage().toString());
 
         event.setCanceled(true);
     }
